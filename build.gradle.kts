9090d7bf2dd53471df639cf67472a618b6879dfd
Malte Esch <accounts@malteesch.de>
2022-07-18 11:20:14 +0200

    feat(server): add acme setup with dns challenge

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,24 @@ val createTraefikCredentialsFile by tasks.registering(EmptyFile::class) {
     }
 }
 
+val createCloudflareDnsApiTokenSecretFile by tasks.registering(EmptyFile::class) {
+    description = "Create cloudflare dns api token secret"
+    file = file("${projectDir}/server/traefik/secrets/cf_dns_api_token.secret")
+    logMessage = true
+    doLast {
+        logger.quiet("Paste your cloudflare dns api token there")
+    }
+}
+
+val createCloudflareZoneApiTokenSecretFile by tasks.registering(EmptyFile::class) {
+    description = "Create cloudflare zone api token secret"
+    file = file("${projectDir}/server/traefik/secrets/cf_zone_api_token.secret")
+    logMessage = true
+    doLast {
+        logger.quiet("Paste your cloudflare zone api token there")
+    }
+}
+
 val startServer by tasks.registering(Exec::class) {
     description = "Start the server stack"
     commandLine = listOf("docker", "compose", "up", "-d", "--remove-orphans")
@@ -20,3 +38,5 @@ val startServer by tasks.registering(Exec::class) {
 }
 
 installServer.dependsOn(createTraefikCredentialsFile)
+installServer.dependsOn(createCloudflareDnsApiTokenSecretFile)
+installServer.dependsOn(createCloudflareZoneApiTokenSecretFile)
